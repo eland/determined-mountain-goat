2842caa596b39da2ce8d6e056afc015b8d282f18
eland <mdanskey@gmail.com>
2013-02-24 15:23:22 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeterminedMountainGoat::Application.config.secret_token = '837a772fc1f750b9eea1325dd533c59578ea9098fa35ff87d00f8c53dd71a29628d12b73df42838c43d94c199a58a90d53809ef2542e0717c3961ca6f2692791'
